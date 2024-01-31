605edd79febd07c4867473832668a3e0b42e0202
Spencer Cho <spenceryoungcho@tamu.edu>
2024-01-30 21:59:58 -0600

    Financial planning

---
+++ Lab_02/financialplanning.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int options;
+    long long int retirement;
+    int daily;
+    int cost;
+
+    vector<pair <int, pair <int, int> > > investments;
+
+    cin >> options;
+    cin >> retirement;
+
+    for (int i = 0; i < options; i++) {
+        cin >> daily;
+        cin >> cost;
+        investments.push_back(make_pair(cost/daily + 1, make_pair(daily, cost)));
+    }
+
+    sort(investments.begin(), investments.end());
+
+    for (int j = 0; j < investments.size(); j++) {
+        cout << "days till profit: " << investments[j].first << ", daily profit: " << investments[j].second.first << ", initial cost: " << investments[j].second.second << endl;
+    }
+
+    int current_day = 1;
+    int current_profit = 0;
+    int daily_profit = 0;
+    int days = 0;
+
+
+    for (int j = 0; j < investments.size(); j++) {
+        if (current_day == investments[j].first) {
+            current_profit += (current_day * investments[j].second.first) - investments[j].second.second;
+            daily_profit += investments[j].second.second;
+            days = ceil(float(retirement - current_profit) / float(daily_profit));
+        }
+
+        if (days > investments[j + 1].first) {
+            current_day = investments[j + 1].first;
+            current_profit += (current_day - days) * daily_profit;
+            current_profit += (current_day * investments[j + 1].second.first) - investments[j + 1].second.second;
+            daily_profit += investments[j + 1].second.second;
+            days = ceil(float(retirement - current_profit) / float(daily_profit));
+        }
+
+        current_day += 1;
+    }
+
+    cout << days << endl;
+}
